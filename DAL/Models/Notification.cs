341958272669085a05f536112c4395760632c98a
devTimmy <timothy.macharia@outlook.com>
2018-02-21 20:29:02 +0300

    security concern edit.

---
+++ DAL/Models/Notification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class Notification : Base
+    {
+        [Required]
+        public Guid AccountId { get; set; }
+    }
+}
